93daae97fae035241ebef9752c82b7d51948c75f
Im-Rises <quentin-morel88@hotmail.com>
2023-04-24 02:19:57 -0400

    Inputs working

--- src/lib.rs
+++ src/lib.rs
@@ -13,27 +13,48 @@ fn main() {
 
     #[wasm_bindgen(start)]
     pub fn start() -> Result<(), JsValue> {
-        let mut space_invaders_arcade = si_arcade::SpaceInvadersArcade::new();
+        let space_invaders_arcade = Rc::new(RefCell::new(si_arcade::SpaceInvadersArcade::new()));
 
         // Set up the keyboard event listener to handle key events
         {
-            let closure =
-                Closure::wrap(Box::new(
-                    move |event: KeyboardEvent, is_pressed: bool| match event.key().as_ref() {
-                        // "ArrowLeft" => space_invaders_arcade.update_input(si_arcade::GameInput::Left, is_pressed),
-                        // "ArrowRight" => space_invaders_arcade.update_input(si_arcade::GameInput::Right, is_pressed),
-                        // "ArrowUp" => space_invaders_arcade.update_input(si_arcade::GameInput::Shot, is_pressed),
-                        // "c" => space_invaders_arcade.update_input(si_arcade::GameInput::Coin, is_pressed),
-                        // "1" => space_invaders_arcade.update_input(si_arcade::GameInput::Player1Start, is_pressed),
-                        // "2" => space_invaders_arcade.update_input(si_arcade::GameInput::Player2Start, is_pressed),
-                        // "k" => space_invaders_arcade.update_input(si_arcade::GameInput::Dip3, is_pressed),
-                        // "l" => space_invaders_arcade.update_input(si_arcade::GameInput::Dip5, is_pressed),
-                        // "m" => space_invaders_arcade.update_input(si_arcade::GameInput::Dip6, is_pressed),
-                        // "j" => space_invaders_arcade.update_input(si_arcade::GameInput::Dip7, is_pressed),
-                        "p" => web_sys::console::log_1(&"Pause".into()),
-                        _ => {}
-                    },
-                ) as Box<dyn FnMut(_, bool)>);
+            let space_invaders_arcade_ref = Rc::clone(&space_invaders_arcade);
+            let closure = Closure::wrap(Box::new(move |event: KeyboardEvent| {
+                let is_pressed = event.type_() == "keydown";
+                match event.key().as_ref() {
+                    "ArrowLeft" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Left, is_pressed),
+                    "ArrowRight" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Right, is_pressed),
+                    "ArrowUp" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Shot, is_pressed),
+                    "c" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Coin, is_pressed),
+                    "1" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Player1Start, is_pressed),
+                    "2" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Player2Start, is_pressed),
+                    "k" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Dip3, is_pressed),
+                    "l" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Dip5, is_pressed),
+                    "m" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Dip6, is_pressed),
+                    "j" => space_invaders_arcade_ref
+                        .borrow_mut()
+                        .update_input(si_arcade::GameInput::Dip7, is_pressed),
+                    "p" => web_sys::console::log_1(&"Pause".into()),
+                    _ => {}
+                }
+            }) as Box<dyn FnMut(_)>);
 
             window().add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref())?;
             window().add_event_listener_with_callback("keyup", closure.as_ref().unchecked_ref())?;
@@ -46,7 +67,8 @@ fn main() {
 
         let mut i = 0;
         *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-            space_invaders_arcade.emulate_cycle();
+            // space_invaders_arcade.emulate_cycle();
+            space_invaders_arcade.borrow_mut().emulate_cycle();
             request_animation_frame(f.borrow().as_ref().unwrap());
         }) as Box<dyn FnMut()>));
 
