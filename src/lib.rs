098cf85780e25fc4cc76477a8ba7c4ebe772676e
Im-Rises <quentin-morel88@hotmail.com>
2023-04-26 01:42:14 -0400

    First version

--- src/lib.rs
+++ src/lib.rs
@@ -20,31 +20,24 @@ pub fn initialize() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn run(canvas_id: String) -> Result<(), JsValue> {
-    /* Debug code */
-    let array_h: [u8; 0x800] = include_bytes!("../game_roms/invaders.h").to_vec().try_into().unwrap();
-    let array_g: [u8; 0x800] = include_bytes!("../game_roms/invaders.g").to_vec().try_into().unwrap();
-    let array_f: [u8; 0x800] = include_bytes!("../game_roms/invaders.f").to_vec().try_into().unwrap();
-    let array_e: [u8; 0x800] = include_bytes!("../game_roms/invaders.e").to_vec().try_into().unwrap();
+pub fn run(
+    canvas_id: String,
+    rom_h: js_sys::Uint8Array,
+    rom_g: js_sys::Uint8Array,
+    rom_f: js_sys::Uint8Array,
+    rom_e: js_sys::Uint8Array,
+) -> Result<(), JsValue> {
+    // /* Debug code */
+    // let array_h: [u8; 0x800] = include_bytes!("../game_roms/invaders.h").to_vec().try_into().unwrap();
+    // let array_g: [u8; 0x800] = include_bytes!("../game_roms/invaders.g").to_vec().try_into().unwrap();
+    // let array_f: [u8; 0x800] = include_bytes!("../game_roms/invaders.f").to_vec().try_into().unwrap();
+    // let array_e: [u8; 0x800] = include_bytes!("../game_roms/invaders.e").to_vec().try_into().unwrap();
 
-    // let input_rom_h = get_input_element("input_rom_h");
-    // let input_rom_g = get_input_element("input_rom_g");
-    // let input_rom_f = get_input_element("input_rom_f");
-    // let input_rom_e = get_input_element("input_rom_e");
-
-    /* Debug code*/
-    // let array_h: [u8; 0x800] = get_rom_from_input(&input_rom_h).unwrap();
-
-    // If the four inputs are filled with the roms
-    // if !input_rom_h.value().is_empty()
-    //     && !input_rom_g.value().is_empty()
-    //     && !input_rom_f.value().is_empty()
-    //     && !input_rom_e.value().is_empty()
     {
-        // let array_h: [u8; 0x800] = get_rom_from_input(&input_rom_h).unwrap();
-        // let array_g: [u8; 0x800] = get_rom_from_input(&input_rom_g).unwrap();
-        // let array_f: [u8; 0x800] = get_rom_from_input(&input_rom_f).unwrap();
-        // let array_e: [u8; 0x800] = get_rom_from_input(&input_rom_e).unwrap();
+        let array_h: [u8; 0x800] = rom_h.to_vec().try_into().unwrap();
+        let array_g: [u8; 0x800] = rom_g.to_vec().try_into().unwrap();
+        let array_f: [u8; 0x800] = rom_f.to_vec().try_into().unwrap();
+        let array_e: [u8; 0x800] = rom_e.to_vec().try_into().unwrap();
 
         // If the four inputs are filled with the roms
         let space_invaders_arcade = Rc::new(RefCell::new(si_arcade::SpaceInvadersArcade::new(
@@ -166,60 +159,3 @@ pub fn test_read_several_uint8array(
 
     Ok(())
 }
-
-// fn get_input_element(id: &str) -> web_sys::HtmlInputElement {
-//     window()
-//         .document()
-//         .unwrap()
-//         .get_element_by_id(id)
-//         .unwrap()
-//         .dyn_into::<web_sys::HtmlInputElement>()
-//         .unwrap()
-// }
-//
-// fn read_file(input: web_sys::HtmlInputElement) {}
-//
-// fn get_rom_from_input(input: &web_sys::HtmlInputElement) -> Result<[u8; 0x800], Error> {
-//     let mut array: [u8; 0x800] = [0; 0x800];
-//
-//     let files = input.files().expect("Error: No file selected.");
-//     let file = files.get(0).expect("Error: No file found.");
-//
-//     web_sys::console::log_1(&format!("file.name = {}", file.name()).into());
-//     web_sys::console::log_1(&format!("file.size = {}", file.size()).into());
-//
-//     let file_reader = web_sys::FileReader::new().unwrap();
-//
-//     // let read_promise = wasm_bindgen_futures::JsFuture::from(file_reader.read_as_array_buffer(&file)).unwrap();
-//     //
-//     // read_promise.then(move |result| {
-//     //     let file_slice = js_sys::Uint8Array::new(&result);
-//     //     // Do something with the file_slice here...
-//     //     web_sys::console::log_1(&format!("file_slice.length = {}", file_slice.length()).into());
-//     //     Ok(())
-//     // })
-//
-//     file_reader.read_as_array_buffer(&file).unwrap();
-//
-//     // let file_slice = js_sys::Uint8Array::new_with_length(file.size() as u32);
-//     let file_slice =
-//         js_sys::Uint8Array::new_with_byte_offset_and_length(&file_reader.result().unwrap(), 0, file.size() as u32);
-//
-//     web_sys::console::log_1(&format!("file_slice.length = {}", file_slice.length()).into());
-//
-//     web_sys::console::log_1(&format!("file_reader_result = {:?}", file_reader.result().unwrap()).into());
-//
-//     if file_slice.length() == 0x800 {
-//         file_slice.copy_to(&mut array[0..0x800]);
-//
-//         for (i, byte) in array.iter().enumerate() {
-//             web_sys::console::log_1(&format!("array_h[{}] = {}", i, byte).into());
-//         }
-//
-//         Ok(array)
-//     } else {
-//         web_sys::console::log_1(&format!("Error: Wrong file size").into());
-//         Err(Error::new("Wrong file size"))
-//     }
-// }
-// }
