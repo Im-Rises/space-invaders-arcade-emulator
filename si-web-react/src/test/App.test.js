7e9f0c58efc1aac41d5720b6fde578c1425ef104
Im-Rises <quentin-morel88@hotmail.com>
2023-05-08 13:32:18 -0400

    wip

--- si-web-react/src/test/App.test.js
+++ si-web-react/src/test/App.test.js
@@ -1,11 +1,10 @@
-// import React from 'react';
-// import {render, screen} from '@testing-library/react';
-// import App from '../App';
-//
-// const {test, expect} = require('@testing-library/react');
-//
-// test('Test render page', () => {
-// 	render(<App/>);
-// 	const linkElement = screen.getByTestId('canvas');
-// 	expect(linkElement).toBeInTheDocument();
-// });
+/* global expect test */
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import App from '../App';
+
+test('Test render page', () => {
+	render(<App/>);
+	const linkElement = screen.getByTestId('canvas');
+	expect(linkElement).toBeInTheDocument();
+});
