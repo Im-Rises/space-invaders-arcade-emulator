1a2466f7691dbc02d15683e76ffc53b6173bcf1a
Im-Rises <quentin-morel88@hotmail.com>
2023-06-01 09:47:08 -0400

    wip

--- src/my_webapi/my_webgl2.rs
+++ src/my_webapi/my_webgl2.rs
@@ -75,7 +75,7 @@ impl MyWebGl2 {
                             // o_outColor = texture(u_texture, v_texcoord);
                             
                             o_outColor = texture(u_texture, v_texcoord);
-                            // o_outColor = texture(u_overlay_texture, v_texcoord);
+                            o_outColor = texture(u_overlay_texture, v_texcoord);
                             
                             // vec4 color = texture(u_texture, v_texcoord);
                             // if (color.rgb == vec3(0.0)) {
@@ -288,23 +288,23 @@ impl MyWebGl2 {
         self.gl.active_texture(WebGl2RenderingContext::TEXTURE0);
         self.gl
             .bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&self.game_texture));
-        self.gl.uniform1i(
-            Some(&self.gl.get_uniform_location(&self.program, "u_texture").unwrap()),
-            0,
-        );
+        // self.gl.uniform1i(
+        //     Some(&self.gl.get_uniform_location(&self.program, "u_texture").unwrap()),
+        //     0,
+        // );
 
         self.gl.active_texture(WebGl2RenderingContext::TEXTURE1);
         self.gl
             .bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&self.overlay_texture));
-        self.gl.uniform1i(
-            Some(
-                &self
-                    .gl
-                    .get_uniform_location(&self.program, "u_overlay_texture")
-                    .unwrap(),
-            ),
-            1,
-        );
+        // self.gl.uniform1i(
+        //     Some(
+        //         &self
+        //             .gl
+        //             .get_uniform_location(&self.program, "u_overlay_texture")
+        //             .unwrap(),
+        //     ),
+        //     1,
+        // );
 
         self.gl
             .draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, self.vertex_count as i32);
