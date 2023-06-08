fc4701828d3148b6763d5cc150dc951d936c86a4
Im-Rises <quentin-morel88@hotmail.com>
2023-06-07 20:27:46 -0400

    wip

--- si-web-react/src/components/SiEmulator.jsx
+++ si-web-react/src/components/SiEmulator.jsx
@@ -13,33 +13,33 @@ const SiEmulator = props => {
 	const siGameCanvasRef = React.useRef(null);
 	const screenPanelRef = React.useRef(null);
 
+	const handleResize = () => {
+		const screenPanel = screenPanelRef.current;
+		const screenPanelWidth = screenPanel.clientWidth;
+		const screenPanelHeight = screenPanel.clientHeight;
+		const screenPanelAspectRatio = screenPanelWidth / screenPanelHeight;
+
+		const backgroundImage = backgroundImageRef.current;
+		const backgroundImageWidth = backgroundImage.clientWidth;
+		const backgroundImageHeight = backgroundImage.clientHeight;
+		const backgroundImageAspectRatio = backgroundImageWidth / backgroundImageHeight;
+
+		if (screenPanelAspectRatio > backgroundImageAspectRatio) {
+			backgroundImageRef.current.style.width = `${screenPanelHeight * backgroundImageAspectRatio}px`;
+			backgroundImageRef.current.style.height = `${screenPanelHeight}px`;
+			siGameCanvasRef.current.style.height = `${screenPanelHeight}px`;
+		} else {
+			backgroundImageRef.current.style.width = `${screenPanelWidth}px`;
+			backgroundImageRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
+			siGameCanvasRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
+		}
+	};
+
 	useEffect(() => {
 		init().then(() => {
 			// run(props.canvasId, props.screenMode, props.oneAdditional, props.twoAdditional, props.earlyUfo, props.coinDemo, props.romDataH, props.romDataG, props.romDataF, props.romDataE);
 			run(props.canvasId, props.screenMode, props.oneAdditional, props.twoAdditional, props.earlyUfo, props.coinDemo);
 		});
-
-		const handleResize = () => {
-			const screenPanel = screenPanelRef.current;
-			const screenPanelWidth = screenPanel.clientWidth;
-			const screenPanelHeight = screenPanel.clientHeight;
-			const screenPanelAspectRatio = screenPanelWidth / screenPanelHeight;
-
-			const backgroundImage = backgroundImageRef.current;
-			const backgroundImageWidth = backgroundImage.clientWidth;
-			const backgroundImageHeight = backgroundImage.clientHeight;
-			const backgroundImageAspectRatio = backgroundImageWidth / backgroundImageHeight;
-
-			if (screenPanelAspectRatio > backgroundImageAspectRatio) {
-				backgroundImageRef.current.style.width = `${screenPanelHeight * backgroundImageAspectRatio}px`;
-				backgroundImageRef.current.style.height = `${screenPanelHeight}px`;
-				siGameCanvasRef.current.style.height = `${screenPanelHeight}px`;
-			} else {
-				backgroundImageRef.current.style.width = `${screenPanelWidth}px`;
-				backgroundImageRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
-				siGameCanvasRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
-			}
-		};
 
 		window.addEventListener('resize', handleResize);
 
@@ -51,7 +51,7 @@ const SiEmulator = props => {
 	return (
 		<div className={'si-emulator'}>
 			<div ref={screenPanelRef} className={'screen-panel'}>
-				<img ref={backgroundImageRef}
+				<img ref={backgroundImageRef} onLoad={handleResize}
 					className={'background-image'}
 					src={props.isBackgroundVisible ? SIBackground : BlackBackground} alt={''}/>
 				<canvas ref={siGameCanvasRef} id={props.canvasId} width={siScreenWidth} height={siScreenHeight}/>
--- si-web-react/src/components/SiGamePanel.jsx
+++ si-web-react/src/components/SiGamePanel.jsx
@@ -10,12 +10,13 @@ const SiGamePanel = () => {
 	const [romDataG, setRomDataG] = useState(null);
 	const [romDataF, setRomDataF] = useState(null);
 	const [romDataE, setRomDataE] = useState(null);
-	const screenModeSelectRef = React.useRef(null);
+	// const screenModeSelectRef = React.useRef(null);
 	const [isBackgroundVisible, setIsBackgroundVisible] = React.useState(true);
 	const oneAdditionalCheckboxRef = React.useRef(null);
 	const twoAdditionalCheckboxRef = React.useRef(null);
 	const earlyUfoCheckboxRef = React.useRef(null);
 	const coinDemoCheckboxRef = React.useRef(null);
+	const [screenMode, setScreenMode] = React.useState('CV');
 
 	return (
 		<>
@@ -23,7 +24,11 @@ const SiGamePanel = () => {
 				!isRomLoaded ? (
 					<div className={'setup-panel'}>
 						<div className={'screen-mode-panel'}>
-							<select id={'screen-mode'} ref={screenModeSelectRef}>
+							<select id={'screen-mode'} onChange={
+								e => {
+									setScreenMode(e.target.value);
+								}
+							}>
 								<option value={'SV'}>Black and white (SV)</option>
 								<option value={'TV'}>Original (TV)</option>
 								<option value={'CV'}>Colored (CV)</option>
@@ -35,12 +40,14 @@ const SiGamePanel = () => {
 								<option value={'hidden'}>Hidden</option>
 							</select>
 							<div>
-								<label><input type={'checkbox'} ref={oneAdditionalCheckboxRef}/>One additional
-                                        life</label>
-								<label><input type={'checkbox'} ref={twoAdditionalCheckboxRef}/>Two additional
-                                        lives</label>
-								<label><input type={'checkbox'} ref={earlyUfoCheckboxRef}/>UFO at 1000 points</label>
-								<label><input type={'checkbox'} ref={coinDemoCheckboxRef}/>Coin in demo</label>
+								<label><input type={'checkbox'} ref={oneAdditionalCheckboxRef}/>
+                                        One additional life</label>
+								<label><input type={'checkbox'} ref={twoAdditionalCheckboxRef}/>
+                                        Two additional lives</label>
+								<label><input type={'checkbox'} ref={earlyUfoCheckboxRef}/>
+                                        UFO at 1000 points</label>
+								<label><input type={'checkbox'} ref={coinDemoCheckboxRef}/>
+                                        Coin in demo</label>
 							</div>
 						</div>
 						<div className={'load-rom-panel'}>
@@ -67,7 +74,7 @@ const SiGamePanel = () => {
 					: (
 						<SiEmulator
 							canvasId={canvasId}
-							screenMode={screenModeSelectRef.current.value}
+							screenMode={screenMode}
 							isBackgroundVisible={isBackgroundVisible}
 							oneAdditional={oneAdditionalCheckboxRef.current.checked}
 							twoAdditional={twoAdditionalCheckboxRef.current.checked}
