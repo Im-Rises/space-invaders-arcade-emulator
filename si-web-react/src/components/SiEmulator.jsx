2767eab2d594c100ebecf04a1b231a33e3657340
Im-Rises <quentin-morel88@hotmail.com>
2023-06-10 00:17:00 -0400

    wip

--- si-web-react/src/components/SiEmulator.jsx
+++ si-web-react/src/components/SiEmulator.jsx
@@ -8,53 +8,20 @@ const SiEmulator = props => {
 	const siScreenWidth = 224;
 	const siScreenHeight = 256;
 
-	const backgroundImageRef = React.useRef(null);
-	const siGameCanvasRef = React.useRef(null);
-	const screenPanelRef = React.useRef(null);
-
-	const handleResize = () => {
-		const screenPanel = screenPanelRef.current;
-		const screenPanelWidth = screenPanel.clientWidth;
-		const screenPanelHeight = screenPanel.clientHeight;
-		const screenPanelAspectRatio = screenPanelWidth / screenPanelHeight;
-
-		const backgroundImage = backgroundImageRef.current;
-		const backgroundImageWidth = backgroundImage.clientWidth;
-		const backgroundImageHeight = backgroundImage.clientHeight;
-		const backgroundImageAspectRatio = backgroundImageWidth / backgroundImageHeight;
-
-		if (screenPanelAspectRatio > backgroundImageAspectRatio) {
-			backgroundImageRef.current.style.width = `${screenPanelHeight * backgroundImageAspectRatio}px`;
-			backgroundImageRef.current.style.height = `${screenPanelHeight}px`;
-			siGameCanvasRef.current.style.height = `${screenPanelHeight}px`;
-		} else {
-			backgroundImageRef.current.style.width = `${screenPanelWidth}px`;
-			backgroundImageRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
-			siGameCanvasRef.current.style.height = `${screenPanelWidth / backgroundImageAspectRatio}px`;
-		}
-	};
-
 	useEffect(() => {
 		init().then(() => {
 			// run(props.canvasId, props.screenMode, props.oneAdditional, props.twoAdditional, props.earlyUfo, props.coinDemo, props.romDataH, props.romDataG, props.romDataF, props.romDataE);
 			run(props.canvasId, props.screenMode, props.oneAdditional, props.twoAdditional, props.earlyUfo, props.coinDemo);// Debug
 		});
-
-		window.addEventListener('resize', handleResize);
-
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
 	}, []);
-
 	return (
 		<div className={'si-emulator'}>
-			<div ref={screenPanelRef} className={'screen-panel'}>
-				<img ref={backgroundImageRef} onLoad={handleResize}
+			<div className={'screen-panel'}>
+				<img
 					className={'background-image'}
 					src={backgroundVersionList.find(backgroundVersion => backgroundVersion.value === props.backgroundVersion).image}
 					alt={'background-img'}/>
-				<canvas ref={siGameCanvasRef} id={props.canvasId} width={siScreenWidth} height={siScreenHeight}/>
+				<canvas id={props.canvasId} width={siScreenWidth} height={siScreenHeight}/>
 			</div>
 			<div className={'control-panel'}>
 				<button id={'si-button-up'}>Up</button>
