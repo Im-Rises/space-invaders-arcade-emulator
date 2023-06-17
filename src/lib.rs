d4f39be2c7ee095ab3901d8a57fd7e75b76ba25e
Im-Rises <quentin-morel88@hotmail.com>
2023-06-16 23:08:44 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ fn init() -> Result<(), JsValue> {
 #[wasm_bindgen]
 pub fn run(
     canvas_id: String,
+    button_id_prefix: String,
     display_mode: String,
     one_extra_life: bool,
     two_extra_lives: bool,
@@ -105,7 +106,8 @@ pub fn run(
     // Set up the HTML buttons callback
     {
         setup_button_callback(
-            "si-button-up",
+            // "si-button-up",
+            (button_id_prefix.clone() + "-up").as_str(),
             si_arcade::GameInput::Shot,
             Rc::clone(&space_invaders_arcade),
         )?;
@@ -113,7 +115,8 @@ pub fn run(
 
     {
         setup_button_callback(
-            "si-button-left",
+            // "si-button-left",
+            (button_id_prefix.clone() + "-left").as_str(),
             si_arcade::GameInput::Left,
             Rc::clone(&space_invaders_arcade),
         )?;
@@ -121,7 +124,7 @@ pub fn run(
 
     {
         setup_button_callback(
-            "si-button-right",
+            (button_id_prefix.clone() + "-right").as_str(),
             si_arcade::GameInput::Right,
             Rc::clone(&space_invaders_arcade),
         )?;
@@ -129,7 +132,7 @@ pub fn run(
 
     {
         setup_button_callback(
-            "si-button-coin",
+            (button_id_prefix.clone() + "-coin").as_str(),
             si_arcade::GameInput::Coin,
             Rc::clone(&space_invaders_arcade),
         )?;
@@ -137,7 +140,7 @@ pub fn run(
 
     {
         setup_button_callback(
-            "si-button-1p",
+            (button_id_prefix.clone() + "-1p").as_str(),
             si_arcade::GameInput::Player1Start,
             Rc::clone(&space_invaders_arcade),
         )?;
@@ -145,7 +148,7 @@ pub fn run(
 
     {
         setup_button_callback(
-            "si-button-2p",
+            (button_id_prefix.clone() + "-2p").as_str(),
             si_arcade::GameInput::Player2Start,
             Rc::clone(&space_invaders_arcade),
         )?;
